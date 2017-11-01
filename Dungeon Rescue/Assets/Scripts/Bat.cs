85d02b1a706319c3ffb6a82dfca5a5139ea70c07
Jeff Buonamici <jeffbuonamici@users.noreply.github.com>
2017-11-01 19:58:24 -0400

    Created Bat enemy

---
+++ Dungeon Rescue/Assets/Scripts/Bat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour {
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.gameObject.name == "Knight") {
+			Destroy (gameObject);
+			col.gameObject.GetComponent<Knight>().TakeDamage(1);
+		}
+	}
+}
---
+++ Dungeon Rescue/Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+	[SerializeField]
+	Transform mTarget;
+	[SerializeField]
+	float mFollowSpeed;
+	[SerializeField]
+	float mFollowRange;
+
+	float mArriveThreshold = 0.05f;
+
+	void Update ()
+	{
+		if(mTarget != null)
+		{
+			// TODO: Make the enemy follow the target "mTarget"
+			//       only if the target is close enough (distance smaller than "mFollowRange")
+			if (Vector2.Distance (transform.position, mTarget.transform.position) < mFollowRange)
+				transform.position += (mTarget.transform.position - transform.position).normalized * Time.deltaTime;
+		}
+	}
+
+	public void SetTarget(Transform target)
+	{
+		mTarget = target;
+	}
+}
